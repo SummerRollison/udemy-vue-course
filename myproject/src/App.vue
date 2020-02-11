b8471d5b8b49ddb07d7db36dd93a6279d1b8b1b1
SummerRollison <me@summerrollison.com>
2020-02-10 21:56:21 -0600

    lesson 18, passing data with props

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,37 +2,35 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
+    <AllFriends :friends="friends" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
+import AllFriends from "./AllFriends";
+import OnlineFriends from "./OnlineFriends";
 export default {
   name: "app",
   components: {
-    Navbar
+    Navbar,
+    AllFriends,
+    OnlineFriends
   },
   data() {
     return {
-      title: "My first Vue app!"
+      title: "My first Vue app!",
+      friends: [
+        { name: "Mario", online: true },
+        { name: "Luigi", online: false },
+        { name: "Toad", online: true },
+        { name: "Bowser", online: false }
+      ]
     };
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1 {
-  font-weight: normal;
-  color: #444;
-  text-align: center;
-}
 </style>
